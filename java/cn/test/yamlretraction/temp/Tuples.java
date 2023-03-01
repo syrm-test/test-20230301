c775745cffd581001756229b4dd0aa704deff301
琉璃 <liuli.dnn@email.cn>
2023-03-01 17:22:45 +0800

    .

---
+++ java/cn/test/yamlretraction/temp/Tuples.java
@@ -0,0 +1,16 @@
+package cn.test.yamlretraction.temp;
+
+/**
+ * A
+ *
+ * @author 冰凝
+ * @date 2023-03-01 17:21
+ */
+@SuppressWarnings("unused")
+public record Tuples<A, B>(A a, B b) {
+
+    public static void m() {
+        System.out.println("m");
+    }
+
+}
