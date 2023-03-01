91ed2c222c9ebba801f4d249190af301711c7693
琉璃 <liuli.dnn@email.cn>
2023-03-01 17:04:43 +0800

    ...

---
+++ src/main/java/cn/test/yamlretraction/YamlRetractionApplication.java
@@ -0,0 +1,13 @@
+package cn.test.yamlretraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlRetractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlRetractionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/test/yamlretraction/YamlRetractionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.test.yamlretraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlRetractionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
